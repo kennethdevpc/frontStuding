18564074069239d6030cf3e72cd6ed48c4cefa04
Kenneth E. Puliche C <kennethdevpc@gmail.com>
2023-09-06 14:26:03 -0500

    adaptacion testimonios con clase

--- react-projects/componentes_clase/testimonios/src/componentes/Testimonio.js
+++ react-projects/componentes_clase/testimonios/src/componentes/Testimonio.js
@@ -1,20 +1,27 @@
 import React from 'react'; //no es necesario usarlo pero  a versiones anteriores si ( si necesita hook y otras cosas si se usa)
 import '../stylesSheets/Testimonio.css';
-
-export function Testimonio(props) {
-  return (
-    <div className="contenedor-testimonio">
-      <img className="imagen-testimonio" src={require('../images/image1.jpg')} alt=" Foto1" />
-      <img className="imagen-testimonio" src={require(`../images/testimonio-${props.imagen}.png`)} alt={`${props.imagen}`} />
-      <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio">
-          <strong>{props.nombre}</strong> en {props.pais}
-        </p>
-        <p className="cargo-testimonio">
-          {props.cargo} en <strong>{props.empresa}</strong>
-        </p>
-        <p className="texto-testimonio">"{props.testimonio}"</p>
+class Testimonio extends React.Component {
+  render() {
+    return (
+      <div className="contenedor-testimonio">
+        <img className="imagen-testimonio" src={require('../images/image1.jpg')} alt=" Foto1" />
+        <img
+          className="imagen-testimonio"
+          src={require(`../images/testimonio-${this.props.imagen}.png`)}
+          alt={`${this.props.imagen}`}
+        />
+        <div className="contenedor-texto-testimonio">
+          <p className="nombre-testimonio">
+            <strong>{this.props.nombre}</strong> en {this.props.pais}
+          </p>
+          <p className="cargo-testimonio">
+            {this.props.cargo} en <strong>{this.props.empresa}</strong>
+          </p>
+          <p className="texto-testimonio">"{this.props.testimonio}"</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export { Testimonio };
